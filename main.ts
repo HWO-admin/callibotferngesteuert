dae161eaf7d30559247d147e6719b1146452f434
HWO-admin <168010263+HWO-admin@users.noreply.github.com>
2024-04-24 18:35:40 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,24 @@ let kanal = 7
 radio.setGroup(7)
 basic.showNumber(kanal)
 basic.forever(function () {
+    let y = 0
     let x = 0
     if (x < 0) {
-        let y = 0
         if (y < 0) {
             calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, Math.abs(x) + y)
             calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, Math.abs(x))
         } else {
             calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, Math.abs(x) - y)
             calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, Math.abs(x))
+        }
+    }
+    if (x > 0) {
+        if (y > 0) {
+            calliBot2.motor(C2Motor.links, C2Dir.rueckwaerts, Math.abs(x) + y)
+            calliBot2.motor(C2Motor.rechts, C2Dir.rueckwaerts, Math.abs(x))
+        } else {
+            calliBot2.motor(C2Motor.rechts, C2Dir.rueckwaerts, Math.abs(x) - y)
+            calliBot2.motor(C2Motor.links, C2Dir.rueckwaerts, Math.abs(x))
         }
     }
 })
